f6fe1e55f60f7547e44fc1280e55b04a0a7ee9eb
spunkprs <prateekraj1988@gmail.com>
2016-03-14 12:23:51 +0530

    Find Duplicates in Array In O(n) time and O(1) space.

---
+++ src/main/java/com/prateek/Arrays/FindDuplicatesInArray.java
@@ -0,0 +1,56 @@
+package com.prateek.Arrays;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.Lists;
+
+public class FindDuplicatesInArray {
+	
+	public Set<Integer> findDuplicates(final int arr[]) {
+		final List<Integer> list = Lists.newArrayList(1, 2, 3, 4);
+		Integer ar[] = (Integer[])list.toArray();
+		return fetchDuplicates(arr);
+	}
+
+	private Set<Integer> fetchDuplicates(final int[] arr) {
+		final Set<Integer> duplicates = new HashSet<Integer>();
+		int frequencyOfZero = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != 0) {
+				if (arr[Math.abs(arr[i])] > 0) {
+					arr[Math.abs(arr[i])] = -arr[Math.abs(arr[i])];
+				} else {
+					duplicates.add(Math.abs(arr[i]));
+				}
+			} else {
+				frequencyOfZero++;
+			}
+		}
+		addZeroToList(duplicates, frequencyOfZero);
+		return duplicates;
+	}
+	
+	/*
+	public int repeatedNumber(final List<Integer> a) {
+	    Integer arr[] = a.toArray(new Integer[a.size()]);
+	    int index = -1;
+	    for (int i = 0; i < arr.length; i++) {
+				if (arr[Math.abs(arr[i])] > 0) {
+					arr[Math.abs(arr[i])] = -arr[Math.abs(arr[i])];
+				} else {
+				    index = i;
+				    break;
+				}
+		}
+		return Math.abs(arr[index]);
+	}
+	*/
+
+	private void addZeroToList(final Set<Integer> duplicates, final int frequencyOfZero) {
+		if (frequencyOfZero > 1) {
+			duplicates.add(0);
+		}
+	}
+}
---
+++ src/test/java/com/prateek/Arrays/FindDuplicatesInArrayTest.java
@@ -0,0 +1,71 @@
+package com.prateek.Arrays;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import org.junit.Assert;
+
+public class FindDuplicatesInArrayTest {
+
+	private FindDuplicatesInArray unit;
+	
+	@Before
+	public void setUp() {
+		unit = new FindDuplicatesInArray();
+	}
+	
+	@Test
+	public void shouldFindDuplicatesWhenThereAreNoZeroesInTheArrayCaseOne() {
+		final int arr[]= {1, 4, 3, 4, 1};
+		
+		final Set<Integer> expectedList = Sets.newHashSet(1, 4);
+		final Set<Integer> actualList = unit.findDuplicates(arr);
+		
+		Assert.assertEquals(expectedList.size(), actualList.size());
+		for (Integer a : expectedList) {
+			Assert.assertTrue(actualList.contains(a));
+		}
+	}
+	
+	@Test
+	public void shouldFindDuplicatesWhenThereAreNoZeroesInTheArrayCaseTwo() {
+		final int arr[]= {1, 4, 3, 4, 4};
+		
+		final Set<Integer> expectedList = Sets.newHashSet(4);
+		final Set<Integer> actualList = unit.findDuplicates(arr);
+		
+		Assert.assertEquals(expectedList.size(), actualList.size());
+		for (Integer a : expectedList) {
+			Assert.assertTrue(actualList.contains(a));
+		}
+	}
+	
+	@Test
+	public void shouldFindDuplicatesWhenThereAreZeroesInTheArray() {
+		final int arr[]= {0, 0, 3, 4, 4};
+		
+		final Set<Integer> expectedList = Sets.newHashSet(4, 0);
+		final Set<Integer> actualList = unit.findDuplicates(arr);
+		
+		Assert.assertEquals(expectedList.size(), actualList.size());
+		for (Integer a : expectedList) {
+			Assert.assertTrue(actualList.contains(a));
+		}
+	}
+	
+	/*
+	@Test
+	public void test() {
+		final List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 4);
+		
+		Assert.assertEquals(4, unit.repeatedNumber(list));
+	}
+	*/
+	
+}
