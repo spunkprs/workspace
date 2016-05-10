9bef6034d036e6997608eef2fd5f0a659cf3c9ea
spunkprs <prateekraj1988@gmail.com>
2016-05-10 07:39:06 +0530

    Changes in TestOne

--- src/main/java/com/prateek/javaconceptsbrushup/TestOne.java
+++ src/main/java/com/prateek/javaconceptsbrushup/TestOne.java
@@ -21,12 +21,25 @@ public class TestOne {
      System.out.println(s9);
      System.out.println(s10);
      
+     String s12 = "XYZ";
+     String s13 = "India";
+     String s14 = s12 + s13;
+     String s15 = "XYZIndia";
+     
+     StringBuffer sb = new StringBuffer("a");
+     sb.append("12345678912345678");
+     System.out.println(sb.length());
+     System.out.println(sb.capacity());
+     
+    
      
      checkForEquality(s3, s4);
      checkForEquality(s4, s6);
      checkForEquality(s4, s7);
      checkForEquality(s4, s8);
      checkForEquality(s4, s11);
+     checkForEquality(s14, s15);
+     System.out.println(s14 == s15);
 	}
 	
 	private static String getResult() {
